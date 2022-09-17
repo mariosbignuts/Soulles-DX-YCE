0ba22f42865ffbe7358e490704c9a2ee1c788c34
Gray <86223857+GrayAnimates@users.noreply.github.com>
2022-09-17 11:30:48 +0700

    Ryoute hirogete!!!! motto zenshin shiyou!!!!! Kao agete!!!!!!! itsumo zenryoku de go!!!!!! Kokoro no junbi ok!!! senobi shitara rirakkusu!!!!! Sooooraaaaaa ni niiiijiiiiiiii o eeeeeegaiiiiiiiiiiiite yuuuuuuukou

--- stages/step.hx
+++ stages/step.hx
@@ -3,6 +3,8 @@ import openfl.utils.Assets;
 import openfl.filters.ShaderFilter;
 // EngineSettings.maxRatingsAllowed = 0;
 EngineSettings.showTimer = false;
+EngineSettings.middleScroll = true;
+EngineSettings.maxRatingsAllowed = 0;
 
 var window = Window;
 
@@ -13,44 +15,144 @@ var window = Window;
 // }
 
 function create() {
-
     FlxG.resizeWindow(1024, 768);
+    PlayState.defaultCamZoom = 1.25;
+
+    bg = new FlxSprite(-92.85, -63).loadGraphic(Paths.image('step/bg'));
+    bg.updateHitbox();
+    bg.antialiasing = true;
+    bg.scrollFactor.set(0, 0);
+    PlayState.add(bg);
+
+    c = new FlxSprite(0, 0).loadGraphic(Paths.image('step/hex'));
+    c.antialiasing = true;
+    c.scrollFactor.set(0, 0);
+    c.screenCenter("X");
+    c.y += 300;
+    c.scale.set(1.8, 1.8);
+    c.color = 0xB8FED6;
+    PlayState.add(c);
+
+    b = new FlxSprite(0, 0).loadGraphic(Paths.image('step/hex'));
+    b.antialiasing = true;
+    b.scrollFactor.set(0, 0);
+    b.screenCenter("X");
+    b.y += 300;
+    b.scale.set(1.3, 1.3);
+    b.color = 0xD3FED6;
+    PlayState.add(b);
+
+    a = new FlxSprite(0, 0).loadGraphic(Paths.image('step/hex'));
+    a.antialiasing = true;
+    a.scrollFactor.set(0, 0);
+    a.screenCenter("X");
+    a.y += 300;
+    a.scale.set(0.8, 0.8);
+    a.color = 0xFFEBFED0;
+    PlayState.add(a);
 
 }
 
-function beatHit(curBeat)
-{
+var boingySproingy:Bool = true;
+var poopFartShittay:Float = 1.5;
+var camRight:Bool = true;
+
+
+function stepHit(curStep:Int) {
+
+    if (boingySproingy){
+      if (curStep % 4 == 0) {
+          FlxTween.tween(PlayState.camHUD, {y: 0}, Conductor.stepCrochet * 0.002, {ease: FlxEase.circOut});
+          FlxTween.tween(PlayState.boyfriend, {y: 275}, Conductor.stepCrochet * 0.002, {ease: FlxEase.quadOut});
+
+      }
+      if (curStep % 4 == 2) {
+          FlxTween.tween(PlayState.camHUD, {y: 15}, Conductor.stepCrochet * 0.002, {ease: FlxEase.sineIn});
+          FlxTween.tween(PlayState.boyfriend, {y: 300}, Conductor.stepCrochet * 0.002, {ease: FlxEase.sineIn});
+      }
+    }
+}
+
+var right:Bool = true;
+
+function beatHit(curBeat) {
+
     switch(PlayState.boyfriend.animation.curAnim.name) {
         case "idle" :
           PlayState.boyfriend.playAnim('idle', true);
       }
+
     switch(PlayState.dad.animation.curAnim.name) {
-        case "idle" :
-          PlayState.dad.playAnim('idle', true);
+      case "idle" :
+        PlayState.dad.playAnim('idle', true);
+    }
+
+      camHUD.zoom += 0.06;
+      camGame.zoom += 0.03;
+
+      
+      if (!PlayState.boyfriend.visible){
+        PlayState.boyfriend.visible = true;
+        PlayState.dad.visible = false;
       }
+      else{
+        PlayState.boyfriend.visible = false;
+        PlayState.dad.visible = true;
+      }
+
+      if (curBeat % 4 == 0) {
+          FlxTween.tween(a, {angle: a.angle - 45}, Conductor.stepCrochet * 0.0024, {ease: FlxEase.sineIn});
+          FlxTween.tween(b, {angle: b.angle + 45}, Conductor.stepCrochet * 0.0024, {ease: FlxEase.sineIn, startDelay: Conductor.stepCrochet * 0.004});
+          FlxTween.tween(c, {angle: c.angle - 45}, Conductor.stepCrochet * 0.0024, {ease: FlxEase.sineIn});
+      }
+
+      if (curBeat % 4 == 2) {
+        FlxTween.tween(a, {angle: a.angle + 45}, Conductor.stepCrochet * 0.0024, {ease: FlxEase.sineIn});
+        FlxTween.tween(b, {angle: b.angle - 45}, Conductor.stepCrochet * 0.0024, {ease: FlxEase.sineIn, startDelay: Conductor.stepCrochet * 0.004});
+        FlxTween.tween(c, {angle: c.angle + 45}, Conductor.stepCrochet * 0.0024, {ease: FlxEase.sineIn});
+    }
+
 }
 
 var strumY:Int = -42;
 
 function createPost() {
 
-  PlayState.dad.visible = false;
-  PlayState.gf.visible = false;
+    PlayState.dad.visible = false;
+    PlayState.gf.visible = false;
+
+    PlayState.boyfriend.scrollFactor.set(0,0);
+    PlayState.boyfriend.x = 450;
+    PlayState.boyfriend.y = 300;
+
+    PlayState.dad.scrollFactor.set(0,0);
+    PlayState.dad.x = 450;
+    PlayState.dad.y = 300;
+  
+    PlayState.dad.addOffset("singLEFT", -38, -6);
+    PlayState.dad.addOffset("singRIGHT", 37, -7);
+    PlayState.dad.addOffset("singUP", 2, 27);
+    PlayState.dad.addOffset("singDOWN", -22, -50);
+
+    PlayState.dad.addOffset("singLEFTmiss", -38, -6);
+    PlayState.dad.addOffset("singRIGHTmiss", 37, -7);
+    PlayState.dad.addOffset("singUPmiss", 2, 27);
+    PlayState.dad.addOffset("singDOWNmiss", -22, -50);
 
   if (EngineSettings.downscroll){
     strumY = 670;
     funnyHud.y = 30;
   }
 
-   for (i in 0...PlayState.cpuStrums.length) {
-       PlayState.cpuStrums.members[i].x -= 42;
-       PlayState.cpuStrums.members[i].y = strumY;
-   }
-
    for (i in 0...PlayState.playerStrums.length) {
          if (!EngineSettings.middleScroll)
          PlayState.playerStrums.members[i].x += 42;
          PlayState.playerStrums.members[i].y = strumY;
+         
+        //  PlayState.playerStrums.members[0].x -= 75;
+        //  PlayState.playerStrums.members[1].x -= 50;
+        //  PlayState.playerStrums.members[2].x += 50;
+        //  PlayState.playerStrums.members[3].x += 75;
    }
 
     PlayState.canDie = false;
@@ -64,50 +166,26 @@ function createPost() {
 
 var ofs1:Int = 30;
 
-function updatePost(elapsed:Float) {
+function update(elapsed:Float) {
 
   PlayState.autoCamZooming = false;
+  PlayState.camFollow.setPosition(640, 480);
 
-//   if (PlayState.section != null && PlayState.section.mustHitSection) {
-//      PlayState.camFollow.setPosition(800, 430);
-//      // PlayState.defaultCamZoom = 1;
-//     PlayState.defaultCamZoom = 1;
+  a.angle += 0.18;
+  b.angle -= 0.18;
+  c.angle += 0.18;
 
-//      switch(PlayState.boyfriend.animation.curAnim.name) {
-//          case "singLEFT":
-//              PlayState.camFollow.x = PlayState.camFollow.x - ofs1;
-//              PlayState.defaultCamZoom = 1.25;
-//          case "singRIGHT":
-//              PlayState.camFollow.x = PlayState.camFollow.x + ofs1;
-//              PlayState.defaultCamZoom = 1.29;
-//          case "singUP":
-//              PlayState.camFollow.y = PlayState.camFollow.y - ofs1;
-//              PlayState.defaultCamZoom = 1.22;
-//          case "singDOWN":
-//              PlayState.camFollow.y = PlayState.camFollow.y + ofs1;
-//              PlayState.defaultCamZoom = 1.23;
-//    }
-//  }
-//    else {
-//      PlayState.camFollow.setPosition(500, 430);
-//      // PlayState.defaultCamZoom = 1;
-//     PlayState.defaultCamZoom = 1.2;
-
-//      switch(PlayState.dad.animation.curAnim.name) {
-//          case "singLEFT":
-//              PlayState.camFollow.x = PlayState.camFollow.x - ofs1;
-//              PlayState.defaultCamZoom = 1.25;
-//          case "singRIGHT":
-//              PlayState.camFollow.x = PlayState.camFollow.x + ofs1;
-//              PlayState.defaultCamZoom = 1.29;
-//          case "singUP":
-//              PlayState.camFollow.y = PlayState.camFollow.y - ofs1;
-//              PlayState.defaultCamZoom = 1.22;
-//          case "singDOWN":
-//              PlayState.camFollow.y = PlayState.camFollow.y + ofs1;
-//              PlayState.defaultCamZoom = 1.23;
-//    }
-//  }
-
+  PlayState.dad.y = PlayState.boyfriend.y;
+  
+  switch(PlayState.boyfriend.animation.curAnim.name) {
+    case "singLEFT":
+      PlayState.dad.playAnim("singRIGHT");
+    case "singRIGHT":
+      PlayState.dad.playAnim("singLEFT");
+    case "singUP":
+      PlayState.dad.playAnim("singUP");
+    case "singDOWN":
+      PlayState.dad.playAnim("singDOWN");
+  }
 
 }
