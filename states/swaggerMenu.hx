609a59e7159246c775c34c7e328eeeb9b93d11b8
Gray <86223857+GrayAnimates@users.noreply.github.com>
2022-10-17 17:17:13 +0700

    idfk how to menu

--- states/swaggerMenu.hx
+++ states/swaggerMenu.hx
@@ -1,34 +1,4 @@
-import LoadingState;
-import flixel.ui.FlxSpriteButton;
-import options.screens.OptionMain;
+//pain,,,,
 
+FlxG.switchState(new FreeplayState());
 
-var songsList = [milk, soulless, sunshine];
-
-function create() {
-    CoolUtil.playMenuMusic();
-    FlxG.resizeWindow(1280, 720);
-
-    for(idx in 0...numbers.length){
-        var anim = numbers[idx];
-        num.animation.add(anim,[idx],0,false);
-    } 
-
-}
-
-function update(elapsed:Float) {
-
-}
-
-function pickCharacter(selectedCharacter) {
-    if (selectedCharacter == sunkBox) {
-        CoolUtil.loadSong(mod, "milk", "normal");
-        LoadingState.loadAndSwitchState(new PlayState_());
-    } else if (selectedCharacter == bfBox) {
-        CoolUtil.loadSong(mod, "sunshine", "normal");
-        LoadingState.loadAndSwitchState(new PlayState_());
-    } else if (selectedCharacter == tailBox) {
-        CoolUtil.loadSong(mod, "soulless", "normal");
-        LoadingState.loadAndSwitchState(new PlayState_());
-    }
-}
--- ui/MainMenuState.hx
+++ ui/MainMenuState.hx
@@ -1,17 +1,17 @@
-// FlxG.switchState(new ModState('swaggerMenu', mod));
+FlxG.switchState(new ModState('swaggerMenu', mod));
 
-// var j:FlxSprite;
+var j:FlxSprite;
 
-// function create() {
+function create() {
   state.options.remove("story mode");
-//   state.options.remove("freeplay");
+  state.options.remove("freeplay");
   state.options.remove("mods");
   state.options.remove("donate");
-//   state.options.remove("toolbox");
+  state.options.remove("toolbox");
   state.options.remove("credits");
-//   state.options.remove("options");
+  state.options.remove("options");
 
 
-// }
+}
 
-// FlxG.mouse.visible = true;
+FlxG.mouse.visible = true;
